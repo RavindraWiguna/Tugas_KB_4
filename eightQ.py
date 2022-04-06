352228ee02f81fdb5269f3f9453fbb5616f1af2d
RavindraWiguna <ravindrawiguna@gmail.com>
2022-04-07 03:41:12 +0700

    fix logic error

--- eightQ.py
+++ eightQ.py
@@ -1,6 +1,7 @@
 # from solution import Solution
 from utils import generate_initial_population, generate_next_population, get_fitness
 
+
 def main():
     print("8 Queens Baby :D")
     # generate populations
@@ -11,18 +12,20 @@ def main():
     isReached = False
     generation = 0
     while(not isReached):
-        current_population = generate_next_population(current_population[0:pick_n_best], 0.5)
         for individu in current_population:
             individu.fitness = get_fitness(individu.chromosome)
 
         current_population.sort(reverse=True)
 
+        # average(current_population)
         print(f'Finished Gen: {generation}| Best Individu: {current_population[0]}')
         generation+=1
         # isReached = True
         if(current_population[0].fitness > -1):
             print("Reached")
             isReached = True
+        else:
+            current_population = generate_next_population(current_population[0:pick_n_best], 1.0)
 
 
 if __name__ =="__main__":
